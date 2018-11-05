09b94058728e9acf7b82c5fd943ffcde8367ac66
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2018-11-05 18:42:33 +0200

    Forgot the package-infos

---
+++ src/main/java/com/cake/utils/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * 05/11/2018 at 18:19:26 ÷.
+ * package-info.java created by Tsvetelin
+ */
+/**
+ * 
+ * The utility package for the compiler.
+ * 
+ * @author Tsvetelin
+ *
+ */
+package com.cake.utils;
