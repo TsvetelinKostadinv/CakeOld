b88122cd6e64b7ac79bfd74364af147e52edcfe6
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2018-12-07 16:21:37 +0200

    Separated reading and logic

--- src/main/java/com/cake/compilation/tokens/types/BaseTypesIdentificators.java
+++ src/main/java/com/cake/compilation/tokens/types/BaseTypesIdentificators.java
@@ -3,7 +3,8 @@
  * BaseTypes.java created by Tsvetelin
  */
 
-package com.cake.tokens.types;
+package com.cake.compilation.tokens.types;
+
 
 /**
  * 
@@ -14,6 +15,7 @@ package com.cake.tokens.types;
  */
 public enum BaseTypesIdentificators
 {
+    //@formatter:off
     EMPTY ( "" ) ,
     OPERATOR ( "OPERATOR") ,
     IDENTIFIER ( "IDENTIFIER") ,
@@ -21,19 +23,24 @@ public enum BaseTypesIdentificators
     NUMBER_LITERAL( "NUMBER_LITERAL") ,
     INTEGER_NUMBER_LITERAL( "INTEGER_LITERAL") ,
     REAL_NUMBER_LITERAL( "REAL_LITERAL"), 
-    BOOLEAN_LITERAL( "BOOLEAN_LITERAL" ) ;
-    
-    
+    BOOLEAN_LITERAL( "BOOLEAN_LITERAL" ),
+    ACCESS_MODIFIER( "ACCESS_MODIFIER" ) ;
+    //@formatter:on
+
+
     private String value = null;
-    
+
+
     /**
      * Private constructor so no one else builds a type
      */
     private BaseTypesIdentificators ( String holder )
     {
+
         this.value = holder;
     }
-    
+
+
     /**
      * @return the value
      */
@@ -42,5 +49,5 @@ public enum BaseTypesIdentificators
 
         return value;
     }
-    
+
 }
